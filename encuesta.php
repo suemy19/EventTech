517b4ac962b657cd77692f119541ec0ab5399b3e
suemy19 <79949757+suemy19@users.noreply.github.com>
2024-11-08 06:41:50 -0500

    Update encuesta.php

--- encuesta.php
+++ encuesta.php
@@ -1,9 +1,8 @@
 <?php
-// Conexión a la base de datos
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "eventos_db";
+$servername = "sql106.infinityfree.com";
+$username = "if0_37675598";
+$password = "Palabra21";
+$dbname = "if0_37675598_XXX";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,33 +11,32 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+echo "Conexión exitosa";
 
-// Verificar si el formulario fue enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $correo = $_POST['correo'];
-    $carrera = $_POST['carrera'];
-    $enteraste = $_POST['enteraste'];
-    $organizacion = $_POST['organizacion'];
-    $interes = $_POST['interes'];
-    $calidad = $_POST['calidad'];
-    $recomendar = $_POST['recomendar'];
-    $mejorar = implode(", ", $_POST['mejorar']); // Para múltiples opciones
-    $participar = $_POST['participar'];
-    $logistica = $_POST['logistica'];
+// Datos del formulario (ejemplo)
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$correo = $_POST['correo'];
+$carrera = $_POST['carrera'];
+$enteraste = $_POST['enteraste'];
+$organizacion = $_POST['organizacion'];
+$interes = $_POST['interes'];
+$calidad_actividades = $_POST['calidad_actividades'];
+$recomendar_evento = $_POST['recomendar_evento'];
+$mejorar = implode(',', $_POST['mejorar']); // Convertir array a string
+$participar = $_POST['participar'];
+$logistica = $_POST['logistica'];
 
-    // Insertar datos en la tabla
-    $sql = "INSERT INTO encuestas (nombre, apellido, correo, carrera, enteraste, organizacion, interes, calidad, recomendar, mejorar, participar, logistica)
-            VALUES ('$nombre', '$apellido', '$correo', '$carrera', '$enteraste', '$organizacion', '$interes', '$calidad', '$recomendar', '$mejorar', '$participar', '$logistica')";
+// Insertar datos en la tabla
+$sql = "INSERT INTO encuesta_tecnologia (nombre, apellido, correo, carrera, enteraste, organizacion, interes, calidad_actividades, recomendar_evento, mejorar, participar, logistica)
+VALUES ('$nombre', '$apellido', '$correo', '$carrera', '$enteraste', '$organizacion', '$interes', '$calidad_actividades', '$recomendar_evento', '$mejorar', '$participar', '$logistica')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Encuesta enviada con éxito.";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+if ($conn->query($sql) === TRUE) {
+    echo "Nuevo registro creado exitosamente";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Cerrar conexión
 $conn->close();
 ?>
+
